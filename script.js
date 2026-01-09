658697b9545c60ea8aba11c9875c22c1bb28168c
ossamasal2012 <ossamasal2012@gmail.com>
2026-01-09 17:11:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const apiKey = "319eb791872b393e9a40b2ea08eb2bc0";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const apiKey = 'YOUR_API_KEY'; // استبدله بمفتاحك الخاص
 const weatherIcons = { 
     'Clear': '☀️', 'Clouds': '☁️', 'Rain': '🌧️', 
     'Drizzle': '🌦️', 'Thunderstorm': '⛈️', 'Snow': '❄️', 'Mist': '🌫️' 
